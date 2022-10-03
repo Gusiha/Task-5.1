564f31b743c3472ee7f7ad6e8c27e396b6ac5c36
TUGARIN <TUGARIN@TUGARIN>
2022-10-03 17:20:46 +0300

    Add project files.

---
+++ Task 5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿int input = 0;
+string result;
+
+if (int.TryParse(Console.ReadLine(), out input))
+{
+    if (Enum.IsDefined(typeof(DayOfWeek), input))
+    {
+        result = Enum.GetName(typeof(DayOfWeek), input);
+    }
+    else
+        throw new IndexOutOfRangeException();
+}
+else
+    throw new Exception("Invalid int value");
+
+Console.WriteLine(result);
